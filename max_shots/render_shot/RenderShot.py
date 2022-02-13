300d5db00517200b0b78bd501469ad2d0f3edca8
xubaolong <xu.baolong@renderg.com>
2022-02-13 17:03:02 +0800

    fix -- 截图命令行加上引号，防止winshell 报错

--- max_shots/render_shot/RenderShot.py
+++ max_shots/render_shot/RenderShot.py
@@ -35,7 +35,6 @@ def xcopy(source, dest):
     subprocess.check_output(cmds, shell=True)
 
 
-
 class RenderShot(object):
     shot_src = os.path.join(RepositoryUtils.GetCustomScriptsDirectory(), "render_shot")
     shot_dst = os.path.join(ClientUtils.GetUsersHomeDirectory(), "render_shot")
@@ -53,7 +52,6 @@ class RenderShot(object):
         self.interval = interval
         self.cur_outfile = ""
         xcopy(self.shot_src, self.shot_dst)
-
 
     def logger(self, msg):
         if callable(self._logger):
@@ -83,54 +81,47 @@ class RenderShot(object):
             self._timer = Timer(self.interval, self.shot_frame_buffer)
             if start:
                 self._timer.start()
-        
 
     def get_shot_outfile(self):
-        if len(self.ExtKyes)!=0:
+        if len(self.ExtKyes) != 0:
             for i in self.ExtKyes:
-                if i == "shot_save_path" :
-                    shot_savepath = self.job.GetJobExtraInfoKeyValue( i )
+                if i == "shot_save_path":
+                    shot_savepath = self.job.GetJobExtraInfoKeyValue(i)
         ##  输出的目录结构为，从提交服务定义的输出地址，加上任务名称，加上shot_.jpg 如: D:/render_shot/2022021202/shot_.jpg
-        if not shot_savepath :
-            return 
+        if not shot_savepath:
+            return
         shot_filesavepath = os.path.join(shot_savepath, self.job.JobName, "shot_.jpg")
         return shot_filesavepath
-
-
-
-
 
     def shot_frame_buffer(self):
         if not SystemUtils.IsRunningOnWindows():
             # self.logger("is not running on windows, do anything!")
             return
 
-
         shot_ext = os.path.join(self.shot_dst, "max_shot.exe")
         shot_out = self.get_shot_outfile()
         shot_out_folder = os.path.dirname(shot_out)
         if not os.path.isdir(shot_out_folder):
             os.makedirs(shot_out_folder)
-        #cmds = [shot_ext, "-f", shot_out.encode(sys.getfilesystemencoding())]
-        cmds = '"{0} -f {1}"'.format(shot_ext, shot_out.encode(sys.getfilesystemencoding()))
+        # cmds = [shot_ext, "-f", shot_out.encode(sys.getfilesystemencoding())]
+        cmds = '"{0}" -f "{1}"'.format(shot_ext, shot_out.encode(sys.getfilesystemencoding()))
 
-        self.logger("Job Name = {}".format( self.job.JobName) )
-        self.logger("shots_savepath = {}".format( shot_out_folder) )
+        self.logger("Job Name = {}".format(self.job.JobName))
+        self.logger("shots_savepath = {}".format(shot_out_folder))
 
         try:
-            
             self.logger("Start to render shot")
-            self.logger("CMDS = {}".format( cmds))
-            self.logger("self.shot_dest = {}".format( self.shot_dst))
-            subprocess.check_output( cmds, stderr=subprocess.PIPE, cwd=self.shot_dst, shell=True)
+            self.logger("CMDS = {}".format(cmds))
+            self.logger("self.shot_dest = {}".format(self.shot_dst))
+            subprocess.check_output(cmds, stderr=subprocess.PIPE, cwd=self.shot_dst, shell=True)
         except subprocess.CalledProcessError:
             pass
         # if not os.path.isfile(shot_out):
         #     self._create_timer()
         #     return
-        
+
         self._create_timer()
-        
-    
+
+
 
 
